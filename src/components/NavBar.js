062f111230655f11dbc9b135272294749862de6c
Sabana <sabanasultana132@gmail.com>
2024-02-12 21:29:07 +0530

    "changes in public folder"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,10 @@ import "./NavBarStyle.css"
 
 export default function NavBar() {
   return (
-    <div>This is my navbar</div>
+    <div>
+      <nav className='NavbarItems'>
+          <h1 className='navbar-logo'>Trippy</h1>
+      </nav>
+    </div>
   )
 }
